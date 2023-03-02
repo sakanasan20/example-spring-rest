3ed0ac08076b00898300752fc8bd1ac0bd8ba0f8
Nick <nick.chen@cmp.com.tw>
2023-03-02 14:11:36 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/spring/rest/ExampleSpringRestApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niq/example/spring/rest/ExampleSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niq.example.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
