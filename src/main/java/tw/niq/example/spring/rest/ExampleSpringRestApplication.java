51ef9c5e7a1cf68f1a1f8e3ffb7d5f981f765680
Nick <nick.chen@cmp.com.tw>
2023-03-14 16:30:55 +0800

    feat: add context path and war config

--- src/main/java/tw/niq/example/spring/rest/ExampleSpringRestApplication.java
+++ src/main/java/tw/niq/example/spring/rest/ExampleSpringRestApplication.java
@@ -3,11 +3,13 @@ package tw.niq.example.spring.rest;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ExampleSpringRestApplication {
+public class ExampleSpringRestApplication extends SpringBootServletInitializer {
 
 	public static ApplicationContext CTX;
 	
@@ -15,6 +17,14 @@ public class ExampleSpringRestApplication {
 		CTX = SpringApplication.run(ExampleSpringRestApplication.class, args);
 	}
 	
+	/**
+	 * For WAR files generation
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ExampleSpringRestApplication.class);
+	}
+
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
